4764e43dee81898897c9feb0aeea5230e3ef6415
sounritesh <sounritesh@gmail.com>
2020-06-30 22:48:15 +0530

    permanent denial handled

--- app/src/main/java/com/curiositymeetsminds/contentproviderexample/MainActivity.kt
+++ app/src/main/java/com/curiositymeetsminds/contentproviderexample/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.curiositymeetsminds.contentproviderexample
 
 import android.Manifest.permission.READ_CONTACTS
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.Settings
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -20,14 +23,19 @@ private const val READ_CONTACTS_REQUEST_CODE = 1
 
 class MainActivity : AppCompatActivity() {
 
-    private var readGranted = false
+//    private var readGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate: starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+//        we are using synthetic import to directly write READ_CONTACTS instead of android.Manifest.permission.READ_CONTACTS
+//        however it is not always a good practice as in case there are many constants then the readability of the code decreases
+//        this is because the programmer is unable to track to which script/file the given constant belongs
         val hasReadContactPermission = ContextCompat.checkSelfPermission(this, READ_CONTACTS)
+
         Log.d(TAG, "onCreate: checkSelfPermission returned $hasReadContactPermission")
 
         if(hasReadContactPermission == PackageManager.PERMISSION_GRANTED){
@@ -39,7 +47,8 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Log.d(TAG, "fab onClick: starts")
-            if (readGranted) {
+//            if (readGranted) {
+            if (ContextCompat.checkSelfPermission(this, READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
                 //creating an array to store strings of column names we need to access, only 1 in this case
                 val projection = arrayOf (ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
                 //making a query to the ContentResolver to access the contacts database, returns a cursor with the requested data
@@ -63,8 +72,24 @@ class MainActivity : AppCompatActivity() {
                 val adapter = ArrayAdapter<String>(this, R.layout.contact_detail, R.id.name, contacts)
                 contactNames.adapter = adapter
             } else {
-                Snackbar.make(view, "Please grant permission to access your contacts.", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                Snackbar.make(view, "Please grant permission to access your contacts.", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Grant Access") {
+                        Log.d(TAG, "Snackbar onClick: starts")
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, READ_CONTACTS)) {
+                            Log.d(TAG, "Snackbar onClick: calling requestPermissions")
+                            ActivityCompat.requestPermissions(this, arrayOf(READ_CONTACTS), READ_CONTACTS_REQUEST_CODE)
+                        } else {
+//                            user has permanently denied permission to access contacts
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts("package", this.packageName, null)
+                            Log.d(TAG, "Uri create: $uri")
+                            intent.data = uri
+                            Log.d(TAG, "Snackbar onClick: launching settings")
+                            this.startActivity(intent)
+                        }
+                        Log.d(TAG, "Snackbar onClick: ends")
+                    }.show()
             }
 
             Log.d(TAG, "fab onClick: ends")
@@ -81,12 +106,13 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onRequestPermissionsResult: starts")
         when (requestCode) {
             READ_CONTACTS_REQUEST_CODE -> {
-                readGranted = if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                readGranted = if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d(TAG, "onRequestPermissionsResult: Permission granted")
-                    true
+//                    true
                 } else {
                     Log.d(TAG, "onRequestPermissionsResult: Permission refused")
-                    false
+//                    false
                 }
 //                disable the floating action button if permission is not granted so
 //                so the app does not crash when the user tries to click the fab
